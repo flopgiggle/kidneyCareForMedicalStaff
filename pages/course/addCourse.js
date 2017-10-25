0b144ed177a5e6f03c9eabb27c45a2a2ee51da88
liwenhai <340416478@qq.com>
2017-10-25 12:34:56 +0800

    go

--- pages/course/addCourse.js
+++ pages/course/addCourse.js
@@ -238,8 +238,8 @@ Page({
             util.http(url,
                 res => {
                     var courseDetail = JSON.parse(res.Result);
-                    courseDetail.StartTimeString = util.formatDate("hh:mm", new Date(courseDetail.StartTime));
-                    courseDetail.EndTimeString = util.formatDate("hh:mm", new Date(courseDetail.EndTime));
+                    courseDetail.StartTimeString = util.formatDate("hh:mm", new Date(courseDetail.StartTime.replace("T", " ")));
+                    courseDetail.EndTimeString = util.formatDate("hh:mm", new Date(courseDetail.EndTime.replace("T", " ")));
                     this.setData({
                         startTime: courseDetail.StartTimeString,
                         endTime: courseDetail.EndTimeString,
